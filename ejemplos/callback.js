9eeaa69b6f0ce4d4a55d9aec8931897f1412bdd6
Stephanie <steaac9@gmail.com>
2024-07-08 22:56:36 -0600

    my lasty

---
+++ ejemplos/callback.js
@@ -0,0 +1,19 @@
+function uno () {
+    console.log("uno")
+    dos()
+}
+
+function dos () {
+    console.log("dos")
+    tres()
+}
+
+setTimeout(uno(), 5000 => {
+    
+    
+    uno()
+    
+    
+    
+    
+}, timeout);
